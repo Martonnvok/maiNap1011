06accbe810012481bd205a179cbe27c5bf9a2383
Martonnvok <martonsom@gmail.com>
2023-10-11 12:43:46 +0200

    Gyakorlás

---
+++ src/tortprogram/Tort.java
@@ -0,0 +1,25 @@
+package tortprogram;
+
+public class Tort {
+
+    private int szamlalo, nevezo;
+
+    public void mutat() {
+        
+    }
+
+    public double ertek() {
+
+        return 0.1;
+    }
+
+    public void reciprokraValt() {
+
+    }
+
+    public Tort reciprokotVisshzaAd() {
+        Tort tort = null;
+
+        return tort;
+    }
+}
